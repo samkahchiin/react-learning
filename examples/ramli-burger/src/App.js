cb95cb50bda8127a3fc7dd66e2ff4a40d71bae39
samkahchiin <samkahchiin@gmail.com>
2020-04-05 22:32:00 +0800

    [Finishes] Added layout and body css

---
+++ examples/ramli-burger/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Layout from './components/Layout/Layout';
+
+function App() {
+  return (
+    <div className="App">
+      <Layout>
+        test
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
