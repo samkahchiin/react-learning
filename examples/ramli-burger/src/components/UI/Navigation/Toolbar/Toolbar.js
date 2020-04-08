4114cc18f353fb30dbbe5994507d6184eec2b02f
samkahchiin <samkahchiin@gmail.com>
2020-04-08 22:33:50 +0800

    [Finishes] Added burger logo

--- examples/ramli-burger/src/components/UI/Navigation/Toolbar/Toolbar.js
+++ examples/ramli-burger/src/components/UI/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import classes from './Toolbar.module.css';
+import Logo from '../../../Logo/Logo';
 
 const toolbar = () => {
   return (
     <header className={classes.Toolbar}>
-      <div>Logo</div>
       <div>Menu</div>
+      <Logo />
       <nav>
       ...
       </nav>
