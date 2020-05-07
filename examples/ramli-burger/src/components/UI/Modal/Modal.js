9d5eb6a5cc72cf8bf5ff4d57bd010f655f1d88f0
samkahchiin <samkahchiin@gmail.com>
2020-05-07 22:37:12 +0800

    [Finishes] Improved the performance by having check in the modal

--- examples/ramli-burger/src/components/UI/Modal/Modal.js
+++ examples/ramli-burger/src/components/UI/Modal/Modal.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.module.css';
 import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-  <Aux>
-    <Backdrop show={props.show} closeModal={props.closeModal}/>
-    <div
-      className={classes.Modal}
-      style={{
-        opacity: props.show ? '1' : '0',
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)'
-      }}
-    >
-      {props.children}
-    </div>
-  </Aux>
-);
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextStates) {
+    return nextProps.show !== this.props.show;
+  }
 
-export default modal;
+  render() {
+    return(
+      <Aux>
+        <Backdrop show={this.props.show} closeModal={this.props.closeModal}/>
+        <div
+          className={classes.Modal}
+          style={{
+            opacity: this.props.show ? '1' : '0',
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)'
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    )
+  }
+}
+
+export default Modal;
