eac9a7065cbd0d0a708a81ac1630967f2b30011b
samkahchiin <samkahchiin@gmail.com>
2020-05-08 20:08:54 +0800

    [Finishes] Added delete requests

--- examples/posts/src/components/FullPost/FullPost.js
+++ examples/posts/src/components/FullPost/FullPost.js
@@ -18,6 +18,11 @@ class FullPost extends Component {
     }
   }
 
+  deletePostHandler = () => {
+    axios.delete("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
+      .then(response => console.log(response))
+  }
+
   render () {
     let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
 
@@ -31,7 +36,7 @@ class FullPost extends Component {
               <h1>{this.state.loadedPost.title}</h1>
               <p>{this.state.loadedPost.body}</p>
               <div className="Edit">
-                  <button className="Delete">Delete</button>
+                  <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
               </div>
           </div>
 
